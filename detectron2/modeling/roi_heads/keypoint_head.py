9b091e370c41d2543f3569c547a8e9415cd5ef71
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-08-13 15:10:24 +0900

    add sigmoid loss

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -155,6 +155,66 @@ def keypoint_rcnn_focalloss(pred_keypoint_logits, instances, normalizer):
 
     return keypoint_loss
 
+
+def keypoint_rcnn_sigmoidloss(pred_keypoint_logits, instances, normalizer):
+    """
+    Arguments:
+        pred_keypoint_logits (Tensor): A tensor of shape (N, K, S, S) where N is the total number
+            of instances in the batch, K is the number of keypoints, and S is the side length
+            of the keypoint heatmap. The values are spatial logits.
+        instances (list[Instances]): A list of M Instances, where M is the batch size.
+            These instances are predictions from the model
+            that are in 1:1 correspondence with pred_keypoint_logits.
+            Each Instances should contain a `gt_keypoints` field containing a `structures.Keypoint`
+            instance.
+        normalizer (float): Normalize the loss by this amount.
+            If not specified, we normalize by the number of visible keypoints in the minibatch.
+
+    Returns a scalar tensor containing the loss.
+    """
+    heatmaps = []
+    valid = []
+
+    keypoint_side_len = pred_keypoint_logits.shape[2]
+    for instances_per_image in instances:
+        if len(instances_per_image) == 0:
+            continue
+        keypoints = instances_per_image.gt_keypoints
+        heatmaps_per_image, valid_per_image = keypoints.to_heatmap(
+            instances_per_image.proposal_boxes.tensor, keypoint_side_len
+        )
+        heatmaps.append(heatmaps_per_image.view(-1))
+        valid.append(valid_per_image.view(-1))
+
+    if len(heatmaps):
+        keypoint_targets = cat(heatmaps, dim=0)
+        valid = cat(valid, dim=0).to(dtype=torch.uint8)
+        valid = torch.nonzero(valid).squeeze(1)
+
+    # torch.mean (in binary_cross_entropy_with_logits) doesn't
+    # accept empty tensors, so handle it separately
+    if len(heatmaps) == 0 or valid.numel() == 0:
+        global _TOTAL_SKIPPED
+        _TOTAL_SKIPPED += 1
+        storage = get_event_storage()
+        storage.put_scalar("kpts_num_skipped_batches", _TOTAL_SKIPPED, smoothing_hint=False)
+        return pred_keypoint_logits.sum() * 0
+
+    N, K, H, W = pred_keypoint_logits.shape
+    pred_keypoint_logits = pred_keypoint_logits.view(N * K, H * W)
+
+
+    input_tmp = pred_keypoint_logits[valid]
+    tareget_tmp = nn.functional.one_hot(keypoint_targets[valid], input_tmp.size()[1]).to(torch.float32)
+    keypoint_loss = F.binary_cross_entropy_with_logits(input_tmp, tareget_tmp, reduction="sum")
+
+    # If a normalizer isn't specified, normalize by the number of visible keypoints in the minibatch
+    if normalizer is None:
+        normalizer = valid.numel()
+    keypoint_loss /= normalizer
+
+    return keypoint_loss
+
 def keypoint_rcnn_inference(pred_keypoint_logits: torch.Tensor, pred_instances: List[Instances]):
     """
     Post process each predicted keypoint heatmap in `pred_keypoint_logits` into (x, y, score)
@@ -215,8 +275,13 @@ class BaseKeypointRCNNHead(nn.Module):
         self.loss_weight = loss_weight
         assert loss_normalizer == "visible" or isinstance(loss_normalizer, float), loss_normalizer
         self.loss_normalizer = loss_normalizer
-        assert loss_type == "normal" or loss_type == "focal" , loss_type
-        self.loss_func = keypoint_rcnn_loss if loss_type=='normal' else keypoint_rcnn_focalloss
+        assert loss_type in ['normal', 'focal', 'sigmoid'], loss_type
+        loss_func_map = {
+            'normal' : keypoint_rcnn_loss,
+            'sigmoid' : keypoint_rcnn_sigmoidloss,
+            'focal' : keypoint_rcnn_focalloss,
+        }
+        self.loss_func = loss_func_map[loss_type]
 
     @classmethod
     def from_config(cls, cfg, input_shape):
